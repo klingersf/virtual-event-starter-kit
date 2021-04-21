cd1429eefd75fdebd61c89fcfa52fc2e9be6ddfe
klingersf <60621086+klingersf@users.noreply.github.com>
2021-04-21 12:29:45 -0300

    Update and rename index.jsx to index.tsx

--- components/ibov/index.tsx
+++ components/ibov/index.tsx
@@ -1,20 +1,23 @@
 import {useRouter} from "next/router";
 import styles from './ibov.module.css';
+import {any} from "prop-types";
 
-export default function IbovImage(ibovData) {
+export default function IbovImage({ ibovData }: any) {
     const { query } = useRouter();
     const up = ibovData[0];
-    //const down = ibovData.filter(isDown);
-    const stock = query.username != "up" ? ibovData[60] : up;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    const down = ibovData.filter(isDown);
+    const stock = query.username != "up" ? down[0] : up;
 
-    //function isDown (value) {
-    //    if (value.resultType == 1)
-    //        return value;
-    //}
+    function isDown (value, index, array) {
+        if (value.resultType == 1)
+            return value;
+    }
 
     return (
         <div className={ stock.resultType == 0 ? styles.bull : styles.bear}>
             <div className={styles.page}>
+                {/*<h2>Teste_ {query.username}</h2>*/}
                 <div className={styles.variation}>{stock.resultPercentageValue}<span>%</span></div>
                 <div className={styles.stockname}>{stock.companyName}</div>
                 <div className={stock.resultType == 0 ? styles.tickerBull : styles.tickerBear}>{stock.code}</div>
