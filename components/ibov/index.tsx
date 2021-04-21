b02033d1d845a41d8c3a6809c775db6a828be000
Klinger <klinger176@gmail.com>
2021-04-21 12:24:37 -0300

    feat: teste

--- components/ibov/index.tsx
+++ components/ibov/index.tsx
@@ -1,13 +1,27 @@
 import {useRouter} from "next/router";
 import styles from './ibov.module.css';
+import {any} from "prop-types";
 
-export default function IbovImage() {
+export default function IbovImage({ ibovData }: any) {
     const { query } = useRouter();
+    const up = ibovData[0];
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    const down = ibovData.filter(isDown);
+    const stock = query.username != "up" ? down[0] : up;
+
+    function isDown (value, index, array) {
+        if (value.resultType == 1)
+            return value;
+    }
+
     return (
-        <div className={styles.background}>
+        <div className={ stock.resultType == 0 ? styles.bull : styles.bear}>
             <div className={styles.page}>
-                <h1> Oi iBov</h1>
-                <h2>Teste_ {query.username}</h2>
+                {/*<h2>Teste_ {query.username}</h2>*/}
+                <div className={styles.variation}>{stock.resultPercentageValue}<span>%</span></div>
+                <div className={styles.stockname}>{stock.companyName}</div>
+                <div className={stock.resultType == 0 ? styles.tickerBull : styles.tickerBear}>{stock.code}</div>
+                <div className={stock.resultType == 0 ? styles.priceBull : styles.priceBear}>{stock.beforeMarket}</div>
             </div>
         </div>
 
