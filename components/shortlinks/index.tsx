3b1f275ae928007610ec18694114c3ed1e74284d
Klinger <klinger176@gmail.com>
2021-04-21 23:21:18 -0300

    bugfix: Links

--- components/shortlinks/index.tsx
+++ components/shortlinks/index.tsx
@@ -7,10 +7,10 @@ export default function ShortLinks() {
     return (
         <div>
             <ul className={styles.mylinks}>
-                <li><a href={'https://2handyn.vercel.app/ibov/up'}>UP - ibov</a></li>
-                <li><a href={'https://2handyn.vercel.app/ibov/down'}>DOWN - ibov</a></li>
-                <li><a href={'https://2handyn.vercel.app/sp500/up'}>UP - sp500</a></li>
-                <li><a href={'https://2handyn.vercel.app/sp500/down'}>DOWN - sp500</a></li>
+                <li><a href={'https://2handyn.vercel.app/api/ibov/up'}>UP - ibov</a></li>
+                <li><a href={'https://2handyn.vercel.app/api/ibov/down'}>DOWN - ibov</a></li>
+                <li><a href={'https://2handyn.vercel.app/api/sp500/up'}>UP - sp500</a></li>
+                <li><a href={'https://2handyn.vercel.app/api/sp500/down'}>DOWN - sp500</a></li>
             </ul>
         </div>
     );
