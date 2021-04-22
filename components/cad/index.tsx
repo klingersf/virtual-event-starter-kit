e42707864f498addbc6b615c280ce960d37d78bb
Klinger <klinger176@gmail.com>
2021-04-22 16:19:30 -0300

    bugfix: StoryCAD

--- components/cad/index.tsx
+++ components/cad/index.tsx
@@ -4,7 +4,7 @@ import {any} from "prop-types";
 import UpArrow from "@components/uparrow";
 import DownArrow from "@components/downarrow";
 
-function getRandomInt({min}: any, {max}: any) {
+function getRandomInt({min}: number, {max}: number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
