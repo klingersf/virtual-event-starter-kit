846caec3dc229fd140ca92a2f7fac5dca1801ce1
Klinger <klinger176@gmail.com>
2021-04-24 11:56:18 -0300

    feat: StockApi

---
+++ pages/api/stocks/[market].tsx
@@ -0,0 +1,65 @@
+import {NextApiRequest, NextApiResponse} from "next";
+
+export default async function MarketApi(req: NextApiRequest, res: NextApiResponse) {
+    let url: string;
+    const { market } = req.query || {};
+    const marketString = market.toString();
+    if (market) {
+
+        let stocksApi = {
+            market: "",
+            type: ""
+        }
+
+        switch (market) {
+            case 'sp500':
+                stocksApi.market = "sp-500";
+                stocksApi.type = "stock";
+                break;
+            case 'ibov':
+                stocksApi.market = "ibovespa";
+                stocksApi.type = "acao";
+            break;
+            default:
+                stocksApi.market = "";
+                stocksApi.type = "";
+        }
+
+        if (!stocksApi.type) {
+
+            res.setHeader(
+                'Cache-Control',
+                `public, s-maxage=3600, max-age=31536000, stale-while-revalidate`
+            );
+            res.statusCode = 404;
+            res.json({
+                error: "Not Found"
+            });
+
+        } else {
+
+            const uri = `https://statusinvest.com.br/${encodeURIComponent(
+                stocksApi.type
+            )}/getaltabaixa?IndiceCode=${encodeURIComponent(
+                stocksApi.market
+            )}`;
+            const resApi = await fetch(uri);
+            const data = await resApi.json();
+            const dynamicDate = new Date();
+
+            res.setHeader(
+                'Cache-Control',
+                `public, s-maxage=10, max-age=86400, stale-while-revalidate`
+            );
+            res.statusCode = 200;
+            res.json({
+                // uri: uri,
+                date: dynamicDate,
+                stocks: data
+            });
+        }
+
+    } else {
+        res.status(404).send('Not Found');
+    }
+}
