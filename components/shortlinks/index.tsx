067ec25f0959e73a17a5a43300592dfc397d156c
Klinger <klinger176@gmail.com>
2021-04-24 21:26:20 -0300

    refactor: add new links

--- components/shortlinks/index.tsx
+++ components/shortlinks/index.tsx
@@ -7,6 +7,8 @@ export default function ShortLinks() {
     return (
         <div>
             <ul className={styles.mylinks}>
+                <li><a href={'https://2handyn.vercel.app/marketday/ibov'}>IBOV</a></li>
+                <li><a href={'https://2handyn.vercel.app/marketday/sp500'}>SP500</a></li>
                 <li><a href={'https://2handyn.vercel.app/api/ibov/up'}>UP - ibov</a></li>
                 <li><a href={'https://2handyn.vercel.app/api/ibov/down'}>DOWN - ibov</a></li>
                 <li><a href={'https://2handyn.vercel.app/api/sp500/up'}>UP - sp500</a></li>
