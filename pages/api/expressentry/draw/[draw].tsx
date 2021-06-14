6492ddf7ac0914e3f23254832d9f56956355acc9
Klinger <klinger176@gmail.com>
2021-06-14 09:44:19 -0300

    feat: teste

--- pages/api/expressentry/draw/[draw].tsx
+++ pages/api/expressentry/draw/[draw].tsx
@@ -2,19 +2,38 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import scrapeDraw from '@lib/scrape-draw';
 import { SITE_URL } from '@lib/constants';
 
-export default function Draw() {
+export default async function getDraw(req: NextApiRequest, res: NextApiResponse) {
 
-    return null
+    const { draw } = req.query || 0;
+    const drawString = draw.toString()
 
-    // const { draw } = req.query || 0;
-    // const drawString = draw.toString()
-    //
+    const dynamicDate = new Date();
+    const inscritos = parseInt(drawString)
+
+    const github = await fetch(`https://api.github.com/users/klingersf`);
+    const githubJson = await github.json();
+
+    res.setHeader('Cache-Control', 's-maxage=10 stale-while-revalidate');
+
+    res.json({
+        date: dynamicDate,
+        inscritos: inscritos,
+        githubJson
+    });
+
+
     // const reponseDraw = await scrapeDraw({draw: parseInt(drawString)});
+    //
+    // return
+    //
+    // res.setHeader('Content-Type', `application/json`);
+    //
     //
     // if (reponseDraw.error) {
     //     res.status(404).send(reponseDraw);
     // }
-    //
+
     // res.statusCode = 200;
     // res.send(reponseDraw)
+    // res.json(reponseDraw)
 }
