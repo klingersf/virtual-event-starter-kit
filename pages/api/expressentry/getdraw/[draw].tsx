9ba3bb1df2b966c57558dadedc47a3667eff8484
Klinger <klinger176@gmail.com>
2021-06-14 08:56:06 -0300

    feat: Scrapping Express Entry Draw

--- pages/api/expressentry/getdraw/[draw].tsx
+++ pages/api/expressentry/getdraw/[draw].tsx
@@ -8,11 +8,17 @@ export default async function getDraw(req: NextApiRequest, res: NextApiResponse)
     const drawString = draw.toString()
 
     const reponseDraw = await scrapeDraw({draw: parseInt(drawString)});
+    //
+    // return
+    //
+    // res.setHeader('Content-Type', `application/json`);
+    //
+    //
+    // if (reponseDraw.error) {
+    //     res.status(404).send(reponseDraw);
+    // }
 
-    if (reponseDraw.error) {
-        res.status(404).send(reponseDraw);
-    }
-
-    res.statusCode = 200;
-    res.send(reponseDraw)
+    // res.statusCode = 200;
+    // res.send(reponseDraw)
+    res.json(reponseDraw)
 }
